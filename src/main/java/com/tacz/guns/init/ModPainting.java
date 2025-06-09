af91b141d277fc415d9c55791cf0676604948803
xjqsh <59763518+xjqsh@users.noreply.github.com>
2025-06-09 12:17:49 +0800

    删除画2

--- src/main/java/com/tacz/guns/init/ModPainting.java
+++ src/main/java/com/tacz/guns/init/ModPainting.java
@@ -10,5 +10,5 @@ public class ModPainting {
     public static final DeferredRegister<PaintingVariant> PAINTINGS = DeferredRegister.create(ForgeRegistries.PAINTING_VARIANTS, GunMod.MOD_ID);
 
     public static final RegistryObject<PaintingVariant> BLOOD_STRIKE_1 = PAINTINGS.register("blood_strike_1", () -> new PaintingVariant(32, 32));
-    public static final RegistryObject<PaintingVariant> BLOOD_STRIKE_2 = PAINTINGS.register("blood_strike_2", () -> new PaintingVariant(32, 32));
+//    public static final RegistryObject<PaintingVariant> BLOOD_STRIKE_2 = PAINTINGS.register("blood_strike_2", () -> new PaintingVariant(32, 32));
 }
