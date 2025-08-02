02476b4bbf3464efa40c6f470be2d7dfa41d2ab6
MUKSC <68376185+MUKSC@users.noreply.github.com>
2025-08-02 10:59:19 +0900

    fix various issues related to hand rendering

--- src/main/java/com/tacz/guns/mixin/client/ItemInHandRendererMixin.java
+++ src/main/java/com/tacz/guns/mixin/client/ItemInHandRendererMixin.java
@@ -1,21 +1,14 @@
 package com.tacz.guns.mixin.client;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.tacz.guns.api.client.event.BeforeRenderHandEvent;
 import com.tacz.guns.api.client.other.KeepingItemRenderer;
 import com.tacz.guns.api.item.IGun;
-import com.tacz.guns.client.event.FirstPersonRenderEvent;
-import com.tacz.guns.compat.iris.IrisCompat;
-import com.tacz.guns.client.renderer.other.HandRenderer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.client.renderer.ItemInHandRenderer;
 import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.common.MinecraftForge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -41,18 +34,7 @@ public class ItemInHandRendererMixin implements KeepingItemRenderer {
 
     @Inject(method = "renderHandsWithItems", at = @At("HEAD"))
     public void beforeHandRender(float pPartialTicks, PoseStack pMatrixStack, MultiBufferSource.BufferSource pBuffer, LocalPlayer pPlayerEntity, int pCombinedLight, CallbackInfo ci) {
-        if (!HandRenderer.INSTANCE.isActive() && !IrisCompat.isPackInUseQuick()) return;
         MinecraftForge.EVENT_BUS.post(new BeforeRenderHandEvent(pMatrixStack));
-    }
-
-    @WrapOperation(method = "renderHandsWithItems", at = @At(value = "INVOKE", target = "Lnet/minecraftforge/client/ForgeHooksClient;renderSpecificFirstPersonHand(Lnet/minecraft/world/InteractionHand;Lcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;IFFFFLnet/minecraft/world/item/ItemStack;)Z", remap = false))
-    private boolean renderHand(InteractionHand hand, PoseStack poseStack, MultiBufferSource bufferSource, int packedLight, float partialTick, float interpPitch, float swingProgress, float equipProgress, ItemStack stack, Operation<Boolean> original) {
-        if (!HandRenderer.INSTANCE.isActive()) {
-            if (IrisCompat.isPackInUseQuick()) FirstPersonRenderEvent.onRenderHand(new RenderHandEvent(hand, poseStack, bufferSource, packedLight, partialTick, interpPitch, swingProgress, equipProgress, stack));
-            return original.call(hand, poseStack, bufferSource, packedLight, partialTick, interpPitch, swingProgress, equipProgress, stack);
-        }
-        FirstPersonRenderEvent.onRenderHand(new RenderHandEvent(hand, poseStack, bufferSource, packedLight, partialTick, interpPitch, swingProgress, equipProgress, stack));
-        return false;
     }
 
     @Inject(method = "tick", at = @At("HEAD"))
