bb44575d4e4ecc699c9dc9cfb25b32d28aa2db4a
MUKSC <68376185+MUKSC@users.noreply.github.com>
2025-12-25 07:20:49 +0900

    events instead of mixins

--- src/main/java/com/tacz/guns/client/event/PreventsHotbarEvent.java
+++ src/main/java/com/tacz/guns/client/event/PreventsHotbarEvent.java
@@ -4,20 +4,25 @@ import com.tacz.guns.client.gui.GunRefitScreen;
 import com.tacz.guns.client.gui.GunSmithTableScreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.Screen;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.client.event.RenderGuiEvent;
 
+@EventBusSubscriber(value = Dist.CLIENT)
 public class PreventsHotbarEvent {
-    public static void onRenderHotbarEvent(CallbackInfo ci) {
+    @SubscribeEvent
+    public static void onRenderHotbarEvent(RenderGuiEvent.Pre event) {
         // todo 需要测试行为
         Screen screen = Minecraft.getInstance().screen;
         // 枪械合成台界面关闭背景
         if (screen instanceof GunSmithTableScreen) {
-            ci.cancel();
+            event.setCanceled(true);
             return;
         }
         // 枪械改装界面关闭背景
         if (screen instanceof GunRefitScreen) {
-            ci.cancel();
+            event.setCanceled(true);
         }
     }
 }
